b10e2c0af2b35dc9c46134ad54edba0ea315fefd
EinatLasry <82314695+EinatLasry@users.noreply.github.com>
2023-05-17 21:44:19 +0300

    Canny
    add edges array in find_equilateral_triangles

--- find_equilateral_triangles.py
+++ find_equilateral_triangles.py
@@ -1,3 +1,33 @@
+import cv2
+import numpy as np
+from math import sqrt, pi, cos, sin, atan2, ceil, floor, radians
+from PIL import Image, ImageDraw
+import random
+
+def find_equilateral_triangles(img, length_side):
+
+    input_image = img
+    # Output image:
+    output_image = Image.new("RGB", input_image.size)
+    output_image.paste(input_image)
+    draw_result = ImageDraw.Draw(output_image)
+
+    height = length_side*0.5*sqrt(3)  # Height in equilateral triangle
+    cols, rows = img.shape
+    points = np.zeros((rows+1, cols+1, 120)) # In equilateral triangle, all sides have the same angle, if we apply modulo 120
+    # 1. canny
+    # 2. calculate gradient and theta
+    edges_arr = cv2.Canny(img, 450, 500)
+    edges_list, gradient, direction = canny_edge_detector_einat(input_image)
+    # binary img
+    edges_arr[edges_arr < 127] = 0
+    edges_arr[edges_arr >= 127] = 255
+    edges = []
+    """ 
+    IN PROCESS
+    """
+
+
 # find points in a specific distance
 def find_points(x, y, m, distance):
     point_b = (x + dx(distance, m), y + dy(distance, m))
